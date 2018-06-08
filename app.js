12a1964e402db225f5f4c98b717e382cee05e86d
Susan Huynh <Susanhuynh@Susan-Huynh.local>
2018-06-08 15:28:06 +0300

    Finish Javascript file

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
 var paint = document.querySelectorAll(".box");
-console.log(paint);
+
+Array.from(paint).forEach(function(element){
+    element.addEventListener("click", function(){
+        var BgColor = element.style.backgroundColor;
+        var smallBox = document.querySelectorAll(".small-box");
+        Array.from(smallBox).forEach(function(e){
+            e.addEventListener("click", function(){
+                if (BgColor === "blue"){
+                    e.style.backgroundColor = "blue";
+                } else if(BgColor === "orangered"){
+                    e.style.backgroundColor = "orangered";
+                } else if (BgColor === "yellow") {
+                    e.style.backgroundColor = "yellow";
+                } else {
+                    e.style.backgroundColor = "green";
+                }
+            })
+        })
+    })
+})
+
+var button = document.querySelector("button");
+var smallBox = document.querySelectorAll(".small-box");
+
+button.addEventListener("click", function(){
+    Array.from(smallBox).forEach(function(el){
+        el.style.backgroundColor = "white";
+    })
+})
+
